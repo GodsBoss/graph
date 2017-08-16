a96fcbafc8648c70c79949762722b131164f1015
Timo Reitz <godsboss@gmx.de>
2017-08-16 20:08:19 +0200

    Begin SimpleGraph implementation.

---
+++ simple_graph.go
@@ -0,0 +1,20 @@
+package graph
+
+// SimpleGraph is a unidirectional non-weighted graph.
+type SimpleGraph struct {
+	nodes NodeSet
+}
+
+// NewSimpleGraph creates a new simple graph.
+func NewSimpleGraph() *SimpleGraph {
+	return &SimpleGraph{
+		nodes: make(NodeSet),
+	}
+}
+
+// AddNode adds a node to the graph and returns that node.
+func (graph *SimpleGraph) AddNode() Node {
+	node := NewNode()
+	graph.nodes[node] = true
+	return node
+}
