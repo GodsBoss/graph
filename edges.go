f6a12889b196546c6afe18a4c5fc0be20148e4be
Timo Reitz <godsboss@gmx.de>
2017-08-16 23:27:20 +0200

    Expand Edge type.

--- edges.go
+++ edges.go
@@ -1,4 +1,26 @@
 package graph
 
 // Edge represents a connection between two nodes.
-type Edge struct{}
+type Edge struct {
+	from Node
+	to   Node
+}
+
+// From returns the edge's from node.
+func (edge Edge) From() Node {
+	return edge.from
+}
+
+// To returns the edge's to node.
+func (edge Edge) To() Node {
+	return edge.to
+}
+
+// Nodes returns the edge's two nodes as a NodeSet. Lets Edge implement
+// NodeExposer.
+func (edge Edge) Nodes() NodeSet {
+	set := NewNodeSet()
+	set.Add(edge.From())
+	set.Add(edge.To())
+	return set
+}
