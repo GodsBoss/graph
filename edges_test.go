5feabdda1cc6abb48b74e2e37f2f3d880a0a9d63
Timo Reitz <godsboss@gmx.de>
2017-08-17 21:41:11 +0200

    Add test for appending edges to edge list.

--- edges_test.go
+++ edges_test.go
@@ -18,3 +18,18 @@ func TestEdgeExposesNodesAsASet(t *testing.T) {
 		t.Errorf("Nodes %+v exposes by edge %+v does not contain %+v", nodes, edge, to)
 	}
 }
+
+func TestEdgesCanBeAppendedToEdgeList(t *testing.T) {
+	edges := graph.NewEdges()
+	n11, n12 := twoNodes()
+	n21, n22 := twoNodes()
+
+	edge1 := graph.NewEdge(n11, n12)
+	edge2 := graph.NewEdge(n21, n22)
+
+	edges.Append(edge1, edge2)
+
+	if edges.Size() != 2 {
+		t.Errorf("Expected size of %+v to be %d, but was %d", edges, 2, edges.Size())
+	}
+}
